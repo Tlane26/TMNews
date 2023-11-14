1fb16ad42e3bfcb50a8701d99ce782ef505cd506
Tlanetzi Chavez <chaveztlanetzi14@gmail.com>
2023-11-14 17:37:50 +0100

    Article View, JsonDecoder

---
+++ TMNews/ArticleRowView.swift
@@ -0,0 +1,84 @@
+//
+//  ArticleRowView.swift
+//  TMNews
+//
+//  Created by Tlanetzi Chavez Madero on 14/11/23.
+//
+
+import SwiftUI
+
+struct ArticleRowView: View {
+    
+   let article: Article
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            AsyncImage(url: article.imageURL){ phase in
+                switch phase {
+                    case .empty:
+                    HStack{
+                        Spacer()
+                        ProgressView()
+                        Spacer()
+                    }
+                        
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                        
+                    case .failure:
+                    HStack{
+                        Spacer()
+                        Image(systemName: "photo")
+                            .imageScale(.large)
+                        Spacer()
+                    }
+                    
+                    @unknown default:
+                        Image(systemName: "photo")
+                }
+            }
+            .frame(minHeight: 200, maxHeight: 300)
+            .background(Color.gray.opacity(0.3))
+            .clipped()
+            
+            VStack(alignment: .leading, spacing: 8){
+                Text(article.source.name)
+                    .foregroundStyle(.red)
+                Text(article.title)
+                    .font(.headline)
+                Text(article.descriptionText)
+                    .font(.subheadline)
+                    .lineLimit(2)
+                
+                HStack{
+                    Text(article.captionText)
+                        .font(.caption)
+                    
+                    Spacer()
+                    
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "bookmark")
+                            .foregroundStyle(.black)
+                    }
+                    .buttonStyle(.bordered)
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+struct ArticleRowView_Previews: PreviewProvider {
+    static var previews: some View{
+        NavigationView {
+            List{
+                ArticleRowView(article: .previewData[0])
+                    .listRowInsets(.init(.init(top: 0, leading: 0, bottom: 0, trailing: 0)))
+            }
+        }
+    }
+}
