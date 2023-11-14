a9cd159c3cc444eafbb4b05488719cc5a8876238
Tlanetzi Chavez <chaveztlanetzi14@gmail.com>
2023-11-14 15:31:31 +0100

    Initial Commit

---
+++ TMNews/TMNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMNewsApp.swift
+//  TMNews
+//
+//  Created by Tlanetzi Chavez Madero on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TMNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
